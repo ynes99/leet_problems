edd67de287943c4b56ca0fbe58a06590a3f1f1bb
ynes99 <72978456+ynes99@users.noreply.github.com>
2022-11-26 01:15:36 +0100

    Time: 4331 ms (19.19%), Space: 18.4 MB (17.76%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -2,21 +2,18 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         positives=[] 
         negatives=[]
-        zeros=0
         output=set()
         nums.sort()
         for num in nums:
-            if num>0:
+            if num>=0:
                 positives.append(num)
             elif num<0:
                 negatives.append(num)    
-            else: 
-                zeros=zeros+1
 
         P, N = set(positives), set(negatives) 
 
         PosTotal=len(positives)
-        negTotal= len(negatives)
+        negTotal=len(negatives)
 
         for a in range(PosTotal):
             for b in range(a+1,PosTotal):
@@ -28,11 +25,8 @@ class Solution:
                 target=(negatives[a]+negatives[b])*-1
                 if target in P:
                     output.add(tuple((target,negatives[a],negatives[b])))
-        if zeros!=0:
-            for p in positives:
-                if -p in N:
-                    output.add((p,-p,0))
-        if zeros>2:
+
+        if nums.count(0)>2:
             output.add((0,0,0))
         
         return output
